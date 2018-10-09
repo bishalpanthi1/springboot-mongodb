ec19ced12e3d3b09b7bf3285a799db72b3c95f14
pradip <rimalpradip31@gmail.com>
2018-10-09 15:45:48 +0545

    project setup

---
+++ src/main/java/com/rest/mongo/MongoApplicationStarter.java
@@ -0,0 +1,11 @@
+package com.rest.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(MongoApplicationStarter.class, args);
+    }
+}
